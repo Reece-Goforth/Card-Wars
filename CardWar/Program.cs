6063a9d2e03d3e379c039e38d277243eeed3d06c
Reece-Goforth <reece.e.goforth06@gmail.com>
2025-07-30 18:15:28 -0400

    Added programmable stats to cards

--- CardWar/Program.cs
+++ CardWar/Program.cs
@@ -77,6 +77,30 @@ namespace CardWar
         }
     }
 
+    struct CardStats
+    {
+        public int Attack;
+        public int Health;
+        public int Defense;
+        public int Evasion;
+
+        public CardStats(int Attack, int Health, int Defense, int Evasion)
+        {
+            this.Attack = Attack;
+            this.Health = Health;
+            this.Defense = Defense;
+            this.Evasion = Evasion;
+        }
+        public override string ToString()
+        {
+            return $"HP: {Health}, ATK: {Attack}, DEF:  {Defense}, EVADE: {Evasion}";
+        }
+        public int GetTotalValue()
+        {
+            return Attack + Health + Defense + Evasion;
+        }
+    }
+
     internal class Program
     {
         public static readonly ConsoleColor DefaultColor = ConsoleColor.White;
@@ -96,7 +120,19 @@ namespace CardWar
             Console.WriteLine($"\nMONEY: {Money}\n");
             Console.ForegroundColor = DefaultColor;
         }
-        static void Main(string[] args)
+        public static void WriteLineColor(string str, ConsoleColor col)
+        {
+            Console.ForegroundColor = col;
+            Console.WriteLine(str);
+            Console.ForegroundColor = DefaultColor;
+        }
+        public static void WriteColor(string str, ConsoleColor col)
+        {
+            Console.ForegroundColor = col;
+            Console.Write(str);
+            Console.ForegroundColor = DefaultColor;
+        }
+        static void Main()
         {
             Deck deck = new Deck(50);
 
@@ -114,6 +150,7 @@ namespace CardWar
                 }
                 if (key.KeyChar == '2')
                 {
+                    Console.Clear();
                     deck.ListCards();
                 }
                 DrawMenu();
@@ -127,7 +164,7 @@ namespace CardWar
         {
             if (!deck.RoomInDeck()) 
             {
-                Console.WriteLine("\nCreate Card failed: No room in deck...");
+                WriteLineColor("\nCreate Card failed: No room in deck...", ConsoleColor.DarkRed);
                 return false;
             }
 
@@ -135,10 +172,12 @@ namespace CardWar
 
             Console.Clear();
         SettingRarity:
+            // Rarity Menu
             DrawMoneyCount();
             Console.WriteLine("Choose Rarity:\n1) Common [$100],\n2) Uncommon [$200]," +
                 "\n3) Rare [$300],\n4) Epic [$400],\n5) Ledgendary [$500]\n6) Mythic [$600]" +
-                "\nB) back to menu");
+                "\nB) back to menu\n");
+            Console.Write("Enter > ");
             ConsoleKeyInfo key = Console.ReadKey();
             string chr = "" + key.KeyChar;
 
@@ -147,46 +186,175 @@ namespace CardWar
                 Console.Clear();
                 return false; 
             }
-
-            int chrNum;
-            if (!int.TryParse(chr, out chrNum) || chrNum < 1 || chrNum > 6)
+            // Extract input character as a number, and perform the necesarry operations
+            if (!int.TryParse(chr, out int chrNum) || chrNum < 1 || chrNum > 6)
             {
-                Console.WriteLine(" is invalid. Must be a number from 1-6...");
+                WriteLineColor(" [INVALID] Must be a number from 1-6...", ConsoleColor.DarkRed);
                 goto SettingRarity;
             }
             if ((chrNum * 100) > Program.Money)
             {
-                Console.WriteLine("\nNot enough funds! Pick something else or return to menu (B)...");
+                WriteLineColor("\nNot enough funds! Pick something else or return to menu (B)...", ConsoleColor.DarkRed);
                 goto SettingRarity;
             }
             newCard.SetCardRarity((CardRarity)chrNum);
 
+            // Set Name of Card
             Console.Clear();
             Console.Write("Name: ");
             string input = Console.ReadLine();
             while (input == "")
             {
-                Console.Write("Name cannot be empty!\n");
+                WriteColor("Name cannot be empty!\n", ConsoleColor.DarkRed);
                 Console.Write("Name: ");
                 input = Console.ReadLine();
             }
             newCard.SetName(input);
 
+            // Set DESC of card
             Console.Clear();
             Console.Write("Description: ");
             input = Console.ReadLine();
             while (input == "")
             {
-                Console.Write("Description cannot be empty!\n");
+                WriteColor("Description cannot be empty!\n", ConsoleColor.DarkRed);
                 Console.Write("Description: ");
                 input = Console.ReadLine();
             }
             newCard.SetDescription(input);
             Console.Clear();
 
+            // Menu for adding stats
+            int maxPoints = chrNum * 100;
+            CardStats newStats = new CardStats();
+
+        SettingStats:
+            Console.WriteLine("[STAT ALLOCATION]\n");
+            Console.WriteLine($"Points used: {newStats.GetTotalValue()}, " +
+                $"Points left: {maxPoints-newStats.GetTotalValue()}\n");
+            Console.WriteLine("Current Stats:");
+            WriteLineColor(newStats.ToString(), newCard.GetRarityColor());
+            Console.WriteLine("\nSelect a stat to modify:\n1) Health (HP)\n2) Attack (ATK)" +
+                "\n3) Defense (DEF)\n4) Evasion (EVADE)\n5) Done");
+            Console.Write("Enter > ");
+
+            key = Console.ReadKey();
+
+            while (!char.IsDigit(key.KeyChar)) 
+            {
+                Console.Clear();
+                WriteLineColor("Invalid input. Type a number from 1-5\n", ConsoleColor.DarkRed);
+                goto SettingStats;
+            }
+
+            switch(key.KeyChar)
+            {
+                default:
+                    Console.Clear();
+                    WriteLineColor("Invalid input. Type a number from 1-5\n", ConsoleColor.DarkRed);
+                    goto SettingStats;
+                case '1':
+                    newStats.Health = 0;
+                    int maxVal = maxPoints - newStats.GetTotalValue();
+                    Console.Clear();
+                    Console.WriteLine($"\nSet Health Points (0-{maxVal}):\n");
+                    Console.Write("Enter > ");
+                    // Get inital input
+                    bool inputIsInt = int.TryParse(Console.ReadLine(), out int intEntered);
+
+                    while (!inputIsInt || intEntered < 0 || intEntered > maxVal)
+                    {
+                        WriteLineColor($"Invalid input, must be a positive integer between 0-{maxVal}.\n", ConsoleColor.DarkRed);
+                        Console.Write("Enter > ");
+
+                        inputIsInt = int.TryParse(Console.ReadLine(), out intEntered);
+                    }
+                    // Successful entry
+                    newStats.Health = intEntered;
+                    Console.Clear();
+                    goto SettingStats;
+                case '2':
+                    newStats.Attack = 0;
+                    maxVal = maxPoints - newStats.GetTotalValue();
+                    Console.Clear();
+                    Console.WriteLine($"\nSet Attack Points (0-{maxVal}):\n");
+                    Console.Write("Enter > ");
+                    // Get inital input
+                    inputIsInt = int.TryParse(Console.ReadLine(), out intEntered);
+
+                    while (!inputIsInt || intEntered < 0 || intEntered > maxVal)
+                    {
+                        WriteLineColor($"Invalid input, must be a positive integer between 0-{maxVal}.\n", ConsoleColor.DarkRed);
+                        Console.Write("Enter > ");
+
+                        inputIsInt = int.TryParse(Console.ReadLine(), out intEntered);
+                    }
+                    // Successful entry
+                    newStats.Attack = intEntered;
+                    Console.Clear();
+                    goto SettingStats;
+                case '3':
+                    newStats.Defense = 0;
+                    maxVal = maxPoints - newStats.GetTotalValue();
+                    Console.Clear();
+                    Console.WriteLine($"\nSet Defense Points (0-{maxVal}):\n");
+                    Console.Write("Enter > ");
+                    // Get inital input
+                    inputIsInt = int.TryParse(Console.ReadLine(), out intEntered);
+
+                    while (!inputIsInt || intEntered < 0 || intEntered > maxVal)
+                    {
+                        WriteLineColor($"Invalid input, must be a positive integer between 0-{maxVal}.\n", ConsoleColor.DarkRed);
+                        Console.Write("Enter > ");
+
+                        inputIsInt = int.TryParse(Console.ReadLine(), out intEntered);
+                    }
+                    // Successful entry
+                    newStats.Defense = intEntered;
+                    Console.Clear();
+                    goto SettingStats;
+                case '4':
+                    newStats.Evasion = 0;
+                    maxVal = maxPoints - newStats.GetTotalValue();
+                    Console.Clear();
+                    Console.WriteLine($"\nSet Evasion Points (1-{maxVal}):\n");
+                    Console.Write("Enter > ");
+                    // Get inital input
+                    inputIsInt = int.TryParse(Console.ReadLine(), out intEntered);
+
+                    while (!inputIsInt || intEntered < 0 || intEntered > maxVal)
+                    {
+                        WriteLineColor($"Invalid input, must be a positive integer between 0-{maxVal}.\n", ConsoleColor.DarkRed);
+                        Console.Write("Enter > ");
+
+                        inputIsInt = int.TryParse(Console.ReadLine(), out intEntered);
+                    }
+                    // Successful entry
+                    newStats.Evasion = intEntered;
+                    Console.Clear();
+                    goto SettingStats;
+                case '5':
+                    if (newStats.GetTotalValue() < maxPoints)
+                    {
+                        Console.Clear();
+                        WriteLineColor($"You still have {maxPoints - newStats.GetTotalValue()} " +
+                            $"unallocated points. Use all of them.\n",  ConsoleColor.DarkRed);
+                        goto SettingStats;
+                    }
+                    if (newStats.Health <= 0)
+                    {
+                        Console.Clear();
+                        WriteLineColor($"Your card must have at least 1 HP", ConsoleColor.DarkRed);
+                        goto SettingStats;
+                    }
+                    goto AddingCard;
+            }
+        AddingCard:
+            Console.Clear();
+            newCard.SetStats(newStats);
             deck.AddCard(newCard);
-            Program.Money -= chrNum*100;
-            Console.WriteLine("Card added to deck...");
+            Program.Money -= maxPoints;
+            WriteLineColor("Card added to deck...", newCard.GetRarityColor());
             return true;
         }
     }
@@ -204,7 +372,7 @@ namespace CardWar
         public void ListCards()
         {
             Console.Write("\nList of cards in deck:");
-            Console.WriteLine($" (Available slots: {maxSize - GetCardCount()})\n");
+            Console.WriteLine($" (Available slots: {maxSize - GetCardCount()}/{maxSize})\n");
             if (cards == null || cards.Count == 0)
             {
                 Console.WriteLine("\tThis deck is empty...");
@@ -213,13 +381,12 @@ namespace CardWar
             SortDeck();
             foreach (Card card in  cards)
             {
-                Console.ForegroundColor = card.GetRarityColor();
-                Console.WriteLine($"\t{card}");
-                Console.ForegroundColor = Program.DefaultColor;
+                Program.WriteLineColor($"\t{card}", card.GetRarityColor());
             }
         }
         public void SortDeck()
         {
+            // Sort by Rarity
             cards = cards.OrderBy(c => c.GetCardRarity()).ToList();
         }
         public int GetCardCount()
@@ -247,12 +414,14 @@ namespace CardWar
         private string Name { get; set; }
         private string Description { get; set; }
         private RarityData Rarity {  get; set; }
+        private CardStats Stats { get; set; }
 
-        public Card(string name, string desc, RarityData rarity)
+        public Card(string name, string desc, RarityData rarity, CardStats stats)
         {
             Name = name;
             Description = desc;
             Rarity = rarity;
+            Stats = stats;
         }
         public Card ()
         {
@@ -261,7 +430,7 @@ namespace CardWar
         public override string ToString()
         {
             return (
-                $"[{GetRarityName()}] {Name}: {Description}"
+                $"[{GetRarityName()}]   [{Name}: {Description}]   [{Stats}]"
             );
         }
         public int GetId() { return Id; }
@@ -279,6 +448,14 @@ namespace CardWar
         public string GetRarityName()
         {
             return Rarity.Name;
+        }
+        public void SetStats(CardStats stats)
+        {
+            Stats = stats;
+        }
+        public CardStats GetStats()
+        {
+            return Stats;
         }
     }
 }
